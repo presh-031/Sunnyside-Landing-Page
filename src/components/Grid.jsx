27f3751ae7d8a71b45104e159840f6c1362d6087
presh-031 <akingbulugbe.dev@gmail.com>
2022-09-16 11:31:36 +0100

    made layout decision within grid component

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,30 +1,41 @@
+import eggImage from "../images/mobile/image-transform.jpg";
+import cupImage from "../images/mobile/image-stand-out.jpg";
+import appleImage from "../images/mobile/image-graphic-design.jpg";
+import orangeImage from "../images/mobile/image-photography.jpg";
+
 const Grid = () => {
   return (
     <section>
       <div>
-        <p>Transform your brand</p>
-        <p>
-          We are a full-service creative agency speciaalizing in helping brands grow fast. Engage your clients through
-          compelling visuals that do most of the marketing for you.
-        </p>
-        <p>LEARN MORE</p>
+        <div className="border-4 text-center border-red-900">
+          <p className=" font-fraunces text-2xl  ">Transform your brand</p>
+          <p>
+            We are a full-service creative agency speciaalizing in helping brands grow fast. Engage your clients through
+            compelling visuals that do most of the marketing for you.
+          </p>
+          <p>LEARN MORE</p>
+        </div>
+        <div>
+          <img src={eggImage} alt="" />
+        </div>
       </div>
       <div>
-        <img src="" alt="" />
+        <div>
+          <img src={cupImage} alt="" />
+        </div>
+        <div>
+          <p>Stand out to the right audience</p>
+          <p>
+            Using a collaborative formula of designers, researchers, photographers , videographers, and copywriters,
+            we'll build and extend your brand in digital places.{" "}
+          </p>
+          <p>LEARN MORE</p>
+        </div>
       </div>
       <div>
-        <img src="" alt="" />
+        <div style={{ backgroundImage: `url(${appleImage})` }}></div>
+        <div style={{ backgroundImage: `url(${orangeImage})` }}></div>
       </div>
-      <div>
-        <p>Stand out to the right audience</p>
-        <p>
-          Using a collaborative formula of designers, researchers, photographers , videographers, and copywriters, we'll
-          build and etend your brand in digital places.{" "}
-        </p>
-        <p>LEARN MORE</p>
-      </div>
-      {/* <div style={{ backgroundImage: `url(${})` }}></div> */}
-      <div></div>
     </section>
   );
 };
