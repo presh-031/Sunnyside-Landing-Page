f9f917f0e68e74c38f0eda5a14abd6e2d91b58c9
presh-031 <akingbulugbe.dev@gmail.com>
2022-09-16 15:46:06 +0100

    finished gallery section, onto footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero";
 import Grid from "./components/Grid";
 import Testimonials from "./components/Testimonials";
+import Gallery from "./components/Gallery";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Hero />
       <Grid />
       <Testimonials />
+      <Gallery />
     </div>
   );
 }
---
+++ src/components/Gallery.jsx
@@ -0,0 +1,25 @@
+import cone from "../images/mobile/image-gallery-cone.jpg";
+import milkBottles from "../images/mobile/image-gallery-milkbottles.jpg";
+import orange from "../images/mobile/image-gallery-orange.jpg";
+import sugarCubes from "../images/mobile/image-gallery-sugar-cubes.jpg";
+
+const Gallery = () => {
+  return (
+    <section className="grid grid-cols-2 mt-24">
+      <div>
+        <img src={milkBottles} alt="" />
+      </div>
+      <div>
+        <img src={orange} alt="" />
+      </div>
+      <div>
+        <img src={cone} alt="" />
+      </div>
+      <div>
+        <img src={sugarCubes} alt="" />
+      </div>
+    </section>
+  );
+};
+
+export default Gallery;
