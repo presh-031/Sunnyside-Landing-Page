741c9299434145839c8fdbd668599acb8f42b3eb
presh-031 <akingbulugbe.dev@gmail.com>
2022-09-17 06:33:09 +0100

    images now responsive,onto misaligned text

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,22 +1,30 @@
-import cone from "../images/mobile/image-gallery-cone.jpg";
-import milkBottles from "../images/mobile/image-gallery-milkbottles.jpg";
-import orange from "../images/mobile/image-gallery-orange.jpg";
-import sugarCubes from "../images/mobile/image-gallery-sugar-cubes.jpg";
+import coneMobile from "../images/mobile/image-gallery-cone.jpg";
+import coneDesktop from "../images/desktop/image-gallery-cone.jpg";
+import milkBottlesMobile from "../images/mobile/image-gallery-milkbottles.jpg";
+import milkBottlesDesktop from "../images/desktop/image-gallery-milkbottles.jpg";
+import orangeMobile from "../images/mobile/image-gallery-orange.jpg";
+import orangeDesktop from "../images/desktop/image-gallery-orange.jpg";
+import sugarCubesMobile from "../images/mobile/image-gallery-sugar-cubes.jpg";
+import sugarCubesDesktop from "../images/desktop/image-gallery-sugarcubes.jpg";
 
 const Gallery = () => {
   return (
     <section className=" mt-24 grid grid-cols-2 md:grid-cols-4">
       <div>
-        <img src={milkBottles} alt="" />
+        <img className="sm:hidden" src={milkBottlesMobile} alt="" />
+        <img className="hidden w-[100%] sm:block" src={milkBottlesDesktop} alt="" />
       </div>
       <div>
-        <img src={orange} alt="" />
+        <img className="sm:hidden" src={orangeMobile} alt="" />
+        <img className="hidden w-[100%] sm:block" src={orangeDesktop} alt="" />
       </div>
+      <desktop>
+        <img className="sm:hidden" src={coneMobile} alt="" />
+        <img className="hidden w-[100%] sm:block" src={coneDesktop} alt="" />
+      </desktop>
       <div>
-        <img src={cone} alt="" />
-      </div>
-      <div>
-        <img src={sugarCubes} alt="" />
+        <img className="sm:hidden" src={sugarCubesMobile} alt="" />
+        <img className="hidden w-[100%] sm:block" src={sugarCubesDesktop} alt="" />
       </div>
     </section>
   );
